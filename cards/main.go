646f9092a922ad04cd99b6ec2389f22c06f78234
Everton Peres <37411123+evertonlperes@users.noreply.github.com>
2022-02-25 17:34:26 -0600

    Section 3 - video 18
    Signed-off-by: Everton Peres <37411123+evertonlperes@users.noreply.github.com>

--- cards/main.go
+++ cards/main.go
@@ -2,18 +2,16 @@ package main
 
 import "fmt"
 
-// Tip: https://play.golang.org/
-
 func main() {
-	// types in go is strict, must define the type.
-	// var card string = "Ace of Spades"
-	card := newCard() // Another way to define a variable in go (recommended)
-	// card = "Five of Diamonds" // Replacing the first value to Five of Diamonds
 
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
-// Go requires to give a type for the function, in this case it will return a type string
 func newCard() string {
 	return "Five of Diamonds"
 }
