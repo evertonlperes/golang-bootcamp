d31dc98d43233a0fda7e1e877de04c91fc4dc46f
Everton Peres <37411123+evertonlperes@users.noreply.github.com>
2022-02-28 12:05:14 -0600

    Added go.mod and minor changes
    Signed-off-by: Everton Peres <37411123+evertonlperes@users.noreply.github.com>

--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	// Printing using the print function
+	// from deck.go
+	cards.print()
 }
 
 func newCard() string {
