c3ffac63e0014a75f41cea03f268864af7071d3f
Everton Peres <37411123+evertonlperes@users.noreply.github.com>
2022-03-02 14:05:43 -0600

    Initial pointer stuff
    Signed-off-by: Everton Peres <37411123+evertonlperes@users.noreply.github.com>

--- struct/main.go
+++ struct/main.go
@@ -12,7 +12,7 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
@@ -20,11 +20,23 @@ func main() {
 	wicks := person{
 		firstName: "Wicks",
 		lastName:  "Testing",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "test@gmail.com",
 			zipCode: "9000",
 		},
 	}
 
-	fmt.Printf("%+v", wicks)
+	wicksPointer := &wicks // &: give the memory address fo the value this variable is pointing at
+	wicksPointer.updateName("Aloww")
+	wicks.print()
+
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName // *pointer: give the value this memory address is pointing at
+}
+
+// Function to print the struct content
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
